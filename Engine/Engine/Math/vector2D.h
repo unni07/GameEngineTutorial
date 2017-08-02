eaf209283ea6a0a3a46924f74634533371f6b754
unni07 <nair.unni07@gmail.com>
2017-08-02 00:08:57 -0400

    + made the ship move using vectors

--- Engine/Engine/Math/vector2D.h
+++ Engine/Engine/Math/vector2D.h
@@ -6,12 +6,27 @@ namespace Math {
 	{
 		float x;
 		float y;
-		vector2D(float i = 0.0f , float j= 0.0f) :x(i), y(j) {}
+		explicit vector2D(float i = 0.0f , float j= 0.0f) :x(i), y(j) {}// explicit will make sure that Compiler will not be type casting anything automatically
 		//vector2D() {}
 	};
 
-	inline vector2D operator+(const vector2D& left, const vector2D& right);
+	/*inline vector2D operator+(const vector2D& left, const vector2D& right);
 	inline vector2D operator*(float scalar, const vector2D& right);
 	inline vector2D operator*(const vector2D& left, float scalar);
-#include "vector2D.inl"
+*/
+
+	vector2D operator+(const vector2D& left, const vector2D& right)
+	{
+		return vector2D(left.x + right.x, left.y + right.y);
+	}
+
+	vector2D operator*(float scalar, const vector2D& right)
+	{
+		return vector2D(scalar * right.x, scalar*right.y);
+	}
+
+	vector2D operator*(const vector2D& left, float scalar)
+	{
+		return scalar * left;
+	}
 }
--- SandboxGame/SandboxGame/MyGlWindow.cpp
+++ SandboxGame/SandboxGame/MyGlWindow.cpp
@@ -2,6 +2,8 @@
 #include <cassert>
 #include "MyGlWindow.h"
 #include <iostream>
+#include <math\vector2D.h>
+using Math::vector2D;
 
 MyGlWindow::MyGlWindow()
 {
@@ -11,25 +13,32 @@ MyGlWindow::MyGlWindow()
 MyGlWindow::~MyGlWindow()
 {
 }
+namespace {
 
-static float verts[] =
-{
-	+0.0f, +0.1f,
-	-0.1f, -0.1f,
-	+0.1f, -0.1f
-};
+	vector2D verts[] =
+	{
+		vector2D(+0.0f, +0.1f),
+		vector2D(-0.1f, -0.1f),
+		vector2D(+0.1f, -0.1f)
+	};
+	static const unsigned int NUM_VERTS = sizeof(verts) / sizeof(*verts);
+	vector2D shipPosition(0.5f, 0.5f);
+	vector2D translatedVerts[NUM_VERTS];
+	vector2D velocity(0.001f, 0.001f);
+}
 
 void MyGlWindow::initializeGL()
 {
 	GLenum errorCode = glewInit();
 	assert(errorCode == 0);
 
+
 	glGenBuffers(1, &vertexBufferID);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
 
 	
 
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), NULL, GL_DYNAMIC_DRAW);
 
 	connect(&myTimer, SIGNAL(timeout()), this, SLOT(myUpdate()));
 	myTimer.start(0);
@@ -42,12 +51,20 @@ void MyGlWindow::paintGL()
 	glClear(GL_COLOR_BUFFER_BIT);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	
+
+	for (unsigned int i = 0; i < NUM_VERTS; i++)
+	{
+		translatedVerts[i] = verts[i] + shipPosition;
+	}
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(translatedVerts), translatedVerts);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 void MyGlWindow::myUpdate()
 {
-	std::cout << "frame" << std::endl;
+	shipPosition = shipPosition + velocity;
+	repaint();
 }
 
 
